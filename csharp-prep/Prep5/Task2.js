4ef991c0f4c3f58d115bb05644b7fc96c77ec58e
johnsonakpama <johnsonakpama33@gmail.com>
2023-01-15 03:30:57 +0000

    task2.html completed

---
+++ csharp-prep/Prep5/Task2.js
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+     myName = "Johnson";
+
+    var myDate = newDate();
+
+    year = myDate. getYear();
+
+    console.log(year);
+
+    var myImage = "images/WIN_20210603_135914-Copy.JPG"
+
+    var favoriteFood = ["potato","rice","yam"]
+
+    console.log(favoriteFood)
+
+    var favoriteFood = []
+
+    favoriteFood.log(favoriteFood.add("bean"));
+
+    console.log(favoriteFood)
+
+    var favoriteFood = []
+
+    favoriteFood.shift()
+
+    console.log(favoriteFood)
+
+    var favoriteFood = []
+    
+    favoriteFood.pop()
+
+    console.log(favoriteFood)
+
+
+    
+} 
