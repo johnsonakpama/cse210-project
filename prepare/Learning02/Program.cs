ba1ce8b198b7a881a77d7721e012338c3bae52b3
johnsonakpama <johnsonakpama33@gmail.com>
2023-01-19 05:31:33 +0000

    I have finish my assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,7 +6,6 @@ class Program
     {
        job job1 = new job();
         job1._jobTitle = "Software Engineer";
-        job1._jobTitle = "Software Engineer";
         job1._compamy = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
@@ -22,24 +21,8 @@ class Program
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
-
-        myResume.Display(); = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        job Job2 = new job();
-        job2._jobTitle = "Manager";
-        job2._compamy = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
-
-        Resume myResume = new Resume();
-        myResume._name = "Johnson Akpama";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display(); 
+        
+         myResume.Display(); 
     }
     
 }
